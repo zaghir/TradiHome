1b43f601ecfafe8931e9a09f70ede3cec52e7bb6
yzaghir <yzaghir@PAR-N-PW07-4807.BETD.FR>
2016-04-19 23:36:24 +0200

    test pour le comit

--- Tradi-home/src/main/java/com/zaghir/dao/TypePersonneRepository.java
+++ Tradi-home/src/main/java/com/zaghir/dao/TypePersonneRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.zaghir.entites.TypePersonne;
 
 public interface TypePersonneRepository extends JpaRepository<TypePersonne, Long> {
-
+ 
 }
